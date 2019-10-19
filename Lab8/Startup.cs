deefc402e09b93ae000a69f8306498a9283bd1e4
David <dcs20@zips.uakon.edu>
2019-10-19 18:49:18 -0400

    lab8 commit

---
+++ Lab8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab8.Startup))]
+namespace Lab8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
