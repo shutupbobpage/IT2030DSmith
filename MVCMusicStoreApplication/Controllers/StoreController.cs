c4fc330de5d8079dd879c6790c7b1aa60467e169
David <dcs20@zips.uakon.edu>
2019-11-03 18:29:14 -0500

    lab10 commit

---
+++ MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,62 @@
+﻿using MVCMusicStoreApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store
+        public ActionResult Index(int id)
+        {
+            
+
+            return View(GetAlbums(id));
+        }
+
+        private List<Album> GetAlbums(int id)
+        {
+            return db.Albums
+                .Where(a => a.GenreId == (id))
+                .ToList();
+        }
+
+        // GET: Browse
+        public ActionResult Browse()
+        {
+            
+            return View(db.Genres.ToList());
+        }
+
+        // GET: Store
+        public ActionResult Details(int id)
+        {
+            return View(GetDetails(id));
+        }
+
+        private List<Album> GetDetails(int id)
+        {
+            return db.Albums
+                .Where(a => a.AlbumId == (id))
+                .ToList();
+        }
+
+        // GET: ShoppingCart
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+
+        // GET: ShoppingCart/AddToCart
+        public ActionResult AddToCart(int id)
+        {
+            return null;
+        }
+    }
+}
